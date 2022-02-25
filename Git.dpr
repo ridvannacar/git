3b0e3c3a00b8d2c6e601052f6b8139ae43f7aa54
ridvannacar <ridvannacar@gmail.com>
2022-02-26 01:32:40 +0300

    İlk denemedir.

---
+++ Git.dpr
@@ -0,0 +1,14 @@
+program Git;
+
+uses
+  Vcl.Forms,
+  Ugit in 'Ugit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
